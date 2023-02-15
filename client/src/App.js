89738c5577a8d60401e5dd4fe51573d997f52aaf
Arbeits Sachen <arbeitsemail000@gmail.com>
2023-02-15 18:06:44 +0000

    Basic Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
+/* eslint-disable linebreak-style */
 import { Route, Routes } from "react-router-dom";
-
 import About from "./pages/About";
 import Home from "./pages/Home";
+import Navbar from "./Navbar";
+import "./App.css";
+
 
 const App = () => (
-	<Routes>
-		<Route path="/" element={<Home />} />
-		<Route path="/about/this/site" element={<About />} />
-	</Routes>
+	<div>
+		<div>
+			<Navbar />
+		</div>
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/about/this/site" element={<About />} />
+		</Routes>
+	</div>
 );
 
 export default App;
