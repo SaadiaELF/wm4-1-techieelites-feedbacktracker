b79c0ea5a3240ccf1f0a58d617d999c2abd71c0e
SaadiaELF <saadiaelfekak@gmail.com>
2023-02-15 13:24:08 +0100

    delete unnecessary files

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-import "./Home.css";
-import logo from "./logo.svg";
 
 export function Home() {
 	const [message, setMessage] = useState("Loading...");
