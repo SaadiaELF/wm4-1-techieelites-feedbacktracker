a3e6a1b2565a1a4a429c038b3f5d1e4bfc195b80
SaadiaELF <saadiaelfekak@gmail.com>
2023-02-15 13:30:00 +0100

    added student dashboard page

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 
 import About from "./pages/About";
 import Home from "./pages/Home";
+import StudentDashboard from "./pages/StudentDashboard";
 
 const App = () => (
 	<Routes>
 		<Route path="/" element={<Home />} />
 		<Route path="/about/this/site" element={<About />} />
+		<Route path="/student" element={<StudentDashboard />} />
 	</Routes>
 );
 
---
+++ client/src/pages/StudentDashboard.js
@@ -0,0 +1,9 @@
+const StudentDashboard = () => (
+	<main role="main">
+		<div>
+			<h1>Student Dashboard</h1>
+		</div>
+	</main>
+);
+
+export default StudentDashboard;
