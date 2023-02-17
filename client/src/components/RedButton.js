f9b63d9c210fe633c6d4bb8a87287832dd11913e
SaadiaELF <saadiaelfekak@gmail.com>
2023-02-17 11:54:19 +0100

    refactoring

--- client/src/components/RedButton.js
+++ client/src/components/RedButton.js
@@ -1,4 +1,3 @@
-
 import Button from "@mui/material/Button";
 import { styled } from "@mui/material/styles";
 
--- client/src/components/WhiteButton.js
+++ client/src/components/WhiteButton.js
@@ -1,6 +1,5 @@
 import Button from "@mui/material/Button";
 import { styled } from "@mui/material/styles";
-import { styled } from "@mui/material/styles";
 
 const WhiteButton = styled(Button)(({ theme }) => ({
 	margin: 5,
