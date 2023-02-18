0f8c2a3392daab7b144204922644097fe2251fca
SaadiaELF <saadiaelfekak@gmail.com>
2023-02-18 01:57:33 +0100

    resolve conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,8 @@
 import { Route, Routes } from "react-router-dom";
-<<<<<<< HEAD
 import About from "./pages/About";
 import Login from "./pages/Login";
-=======
-import { createTheme } from "@mui/material/styles";
-
-import About from "./pages/About";
-import Home from "./pages/Home";
 import StudentDashboard from "./pages/StudentDashboard";
+import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
 	components: {
@@ -18,19 +13,15 @@ const theme = createTheme({
 		},
 	},
 });
->>>>>>> dev
 
 const App = () => (
 	<div className="App">
-		
-	<Routes>
-		<Route path="/" element={<Login />} />
-		<Route path="/about/this/site" element={<About />} />
-		<Route path="/student" element={<StudentDashboard theme={theme} />} />
-	</Routes>
+		<Routes>
+			<Route path="/" element={<Login />} />
+			<Route path="/about/this/site" element={<About />} />
+			<Route path="/student" element={<StudentDashboard theme={theme} />} />
+		</Routes>
 	</div>
 );
 
 export default App;
-
-
