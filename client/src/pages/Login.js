bd9c9b9fc993816945065836eade21589254461c
JanefrancessC <janefrancesschioma@gmail.com>
2023-02-18 16:52:01 +0000

    added a container

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -20,68 +20,72 @@ const handlePasswordVisibility = () => {
 }
 
 	return (
-		<Stack sx={{ maxWidth: 360, padding: "1rem" }} spacing={2}>
-			<Stack >
-				<Typography variant="h4" sx={{ fontSize: 32 }}>
-					Hi, Welcome back! 👋
-				</Typography>
-				<Typography variant="h6" sx={{ fontSize: 20 }}>
-					Hello again, you have been missed
-				</Typography>
-			</Stack>
-			<form onSubmit={handleSubmit}>
-				<Stack spacing={2}>
-					<TextField
-						variant="outlined"
-						label="Email"
-						type="email"
-						fullWidth
-						id="email"
-						name="email"
-						placeholder="example@gmail.com"
-						required
-						onChange={(e) => setValues({ ...values, email: e.target.value })}
-					></TextField>
-
-					<TextField
-						variant="outlined"
-						label="Password"
-						type={values.showPassword ? "text" : "password"}
-						fullWidth
-						id="password"
-						name="password"
-						placeholder="********"
-						required
-						onChange={(e) => setValues({ ...values, password: e.target.value })}
-						InputProps={{
-							endAdornment: (
-								<InputAdornment position="end">
-									<IconButton
-										onClick={handlePasswordVisibility}
-										aria-label="toggle password"
-										edge="end"
-									>
-										{values.showPassword ? (
-											<VisibilityOffIcon />
-										) : (
-											<VisibilityIcon />
-										)}
-									</IconButton>
-								</InputAdornment>
-							),
-						}}
-					></TextField>
-
-					<Button
-						variant="contained"
-						fullWidth
-						style={{ backgroundColor: "#EE4344" }}
-					>
-						Login
-					</Button>
+		<div style={{marginInline: 'auto', maxWidth: '72rem'}}>
+			<Stack sx={{ maxWidth: 360, padding: "1rem" }} spacing={2}>
+				<Stack>
+					<Typography variant="h4" sx={{ fontSize: 32 }}>
+						Hi, Welcome back! 👋
+					</Typography>
+					<Typography variant="h6" sx={{ fontSize: 20 }}>
+						Hello again, you have been missed
+					</Typography>
 				</Stack>
-			</form>
-		</Stack>
+				<form onSubmit={handleSubmit}>
+					<Stack spacing={2}>
+						<TextField
+							variant="outlined"
+							label="Email"
+							type="email"
+							fullWidth
+							id="email"
+							name="email"
+							placeholder="example@gmail.com"
+							required
+							onChange={(e) => setValues({ ...values, email: e.target.value })}
+						></TextField>
+
+						<TextField
+							variant="outlined"
+							label="Password"
+							type={values.showPassword ? "text" : "password"}
+							fullWidth
+							id="password"
+							name="password"
+							placeholder="********"
+							required
+							onChange={(e) =>
+								setValues({ ...values, password: e.target.value })
+							}
+							InputProps={{
+								endAdornment: (
+									<InputAdornment position="end">
+										<IconButton
+											onClick={handlePasswordVisibility}
+											aria-label="toggle password"
+											edge="end"
+										>
+											{values.showPassword ? (
+												<VisibilityOffIcon />
+											) : (
+												<VisibilityIcon />
+											)}
+										</IconButton>
+									</InputAdornment>
+								),
+							}}
+						></TextField>
+
+						<Button
+							variant="contained"
+							fullWidth
+							style={{ backgroundColor: "#EE4344" }}
+						>
+							Login
+						</Button>
+					</Stack>
+				</form>
+			</Stack>
+		</div>
 	);
 }
 
