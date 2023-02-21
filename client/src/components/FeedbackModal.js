d0e122f4274d517010a9f71150a56e5643296812
SaadiaELF <saadiaelfekak@gmail.com>
2023-02-21 12:53:34 +0100

    added feedback modal

---
+++ client/src/components/FeedbackModal.js
@@ -0,0 +1,5 @@
+const FeedbackModal = () => {
+	return <></>;
+};
+
+export default FeedbackModal;
--- client/src/pages/StudentDashboard.js
+++ client/src/pages/StudentDashboard.js
@@ -3,12 +3,14 @@ import { ThemeProvider } from "@mui/material/styles";
 import Stack from "@mui/material/Stack";
 import WelcomeMsg from "../components/WelcomeMsg";
 import Profile from "../components/Profile";
+import FeedbackModal from "../components/FeedbackModal";
 
 const StudentDashboard = ({ theme }) => (
 	<ThemeProvider theme={theme}>
 		<Stack sx={{ maxWidth: "380px", padding: "1rem", margin: "auto" }}>
 			<WelcomeMsg message="Welcome student name!👋" />
 			<Profile />
+			<FeedbackModal />
 		</Stack>
 	</ThemeProvider>
 );
