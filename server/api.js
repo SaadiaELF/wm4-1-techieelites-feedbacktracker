d094c2c02be5cfea67ed84f6a683d2c647b03041
JanefrancessC <janefrancesschioma@gmail.com>
2023-02-23 12:47:56 +0000

    creating endpoints

--- server/api.js
+++ server/api.js
@@ -1,4 +1,5 @@
-import { Router } from "express";
+import { response, Router } from "express";
+import db from "./db";
 
 import logger from "./utils/logger";
 
@@ -9,4 +10,55 @@ router.get("/", (_, res) => {
 	res.json({ message: "Hello, world!" });
 });
 
+router.get("/users", async (req, res) => {
+	try {
+		
+		const users = await db.query(
+			"SELECT * FROM users"
+		);
+		if (!users) {
+			return res.status(404).json({ message: "No users found" });
+		}
+		res.json(users.rows);
+	} catch (err) {
+		console.error(err);
+	}
+});
+
+router.get("/users/:id", async (req, res) => {
+	try {
+		const userId = parseInt(req.params.id);
+		const users = await db.query(
+			"SELECT student_id AS id, full_name, role FROM students UNION ALL SELECT mentor_id, full_name, role FROM mentors"
+		);
+		if (!users) {
+			res.status(404).json({ message: "No users found" });
+		}
+	} catch (err) {
+		console.error(err);
+	}
+});
+
+//post
+
+router.post("/users", async (req, res) => {
+	try {
+	} catch (err) {
+		console.error(err);
+	}
+});
+
+router.put("/users/:id", async (req, res) => {
+	try {
+	} catch (err) {
+		console.error(err);
+	}
+});
+
+router.delete("/users/:id", async (req, res) => {
+	try {
+	} catch (err) {
+		console.error(err);
+	}
+});
 export default router;
