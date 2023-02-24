70937cb0cd8bbdb85e1447ced12404790668daf4
Arbeits Sachen <arbeitsemail000@gmail.com>
2023-02-24 18:01:53 +0000

    Fixed Admin Table

--- server/database.sql
+++ server/database.sql
@@ -18,7 +18,15 @@ CREATE TABLE users (
     CONSTRAINT users_pkey PRIMARY KEY (user_id)
 )
 
-CREATE TABLE admins (admin_id INT REFERENCES users(id));
+CREATE TABLE admins (
+    admin_id integer NOT NULL DEFAULT 'nextval('admins_admin_id_seq'::regclass)',
+    CONSTRAINT admins_pkey PRIMARY KEY (admin_id),
+    CONSTRAINT user_id FOREIGN KEY (admin_id)
+        REFERENCES public.users (user_id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+        NOT VALID
+)
 
 CREATE TABLE mentors (
     mentor_id integer NOT NULL DEFAULT 'nextval('mentors_mentor_id_seq'::regclass)',
@@ -29,6 +37,7 @@ CREATE TABLE mentors (
         ON DELETE CASCADE
         NOT VALID
 )
+
 CREATE TABLE students (
     student_id integer NOT NULL DEFAULT 'nextval('students_student_id_seq'::regclass)',
     module character varying(120) COLLATE pg_catalog."default",
