1340a62f026b7d2926a6d3d3d1f6c758fa6a040c
Arbeits Sachen <arbeitsemail000@gmail.com>
2023-02-27 14:18:17 +0000

    Added Module Table

--- server/database.sql
+++ server/database.sql
@@ -1,4 +1,25 @@
+DDROP TABLE IF EXISTS module;
+
+
+
 DROP TABLE IF EXISTS users;
+
+
+
+
+CREATE TABLE module
+(
+    module_id integer NOT NULL,
+    title character varying COLLATE pg_catalog."default",
+    lessons character varying[] COLLATE pg_catalog."default",
+    module_url character varying COLLATE pg_catalog."default",
+    CONSTRAINT admins_pkey PRIMARY KEY (module_id),
+    CONSTRAINT user_id FOREIGN KEY (module_id)
+        REFERENCES users (user_id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+        NOT VALID
+)
 
 
 
