b18b8c997d02368354c33f69e122d3384a3c5d96
SaadiaELF <saadiaelfekak@gmail.com>
2023-02-27 20:49:15 +0100

    refactored profile

--- client/src/pages/StudentDashboard.js
+++ client/src/pages/StudentDashboard.js
@@ -6,22 +6,35 @@ import Profile from "../components/Profile";
 import FeedbackModal from "../components/FeedbackModal";
 import Progress from "../components/Progress";
 
-const StudentDashboard = ({ theme }) => (
-	<ThemeProvider theme={theme}>
-		<Stack
-			sx={{
-				maxWidth: "380px",
-				padding: "1rem",
-				margin: "auto",
-			}}
-			spacing={2}
-		>
-			<WelcomeMsg message="Welcome student name!👋" />
-			<Profile />
-      <Progress />
-			<FeedbackModal />
-		</Stack>
-	</ThemeProvider>
-);
+const StudentDashboard = ({ theme }) => {
+	const [bio, setBio] = React.useState(
+		"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec consequat ipsum."
+	);
+
+	const handleBioChange = (e) => {
+		setBio(e.target.value);
+	};
+	return (
+		<ThemeProvider theme={theme}>
+			<Stack
+				sx={{
+					maxWidth: "380px",
+					padding: "1rem",
+					margin: "auto",
+				}}
+				spacing={2}
+			>
+				<WelcomeMsg message="Welcome student name!👋" />
+				<Profile
+					mentorName="mentor name"
+					bio={bio}
+					handleBioChange={handleBioChange}
+				/>
+				<Progress />
+				<FeedbackModal />
+			</Stack>
+		</ThemeProvider>
+	);
+};
 
 export default StudentDashboard;
