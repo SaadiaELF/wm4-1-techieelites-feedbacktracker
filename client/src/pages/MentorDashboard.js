0493bd6ccfd510ccdb72e8dd4bc5fb9fc762ae1d
JanefrancessC <janefrancesschioma@gmail.com>
2023-03-03 12:18:53 +0000

    fetching data

--- client/src/pages/MentorDashboard.js
+++ client/src/pages/MentorDashboard.js
@@ -4,8 +4,50 @@ import Stack from "@mui/material/Stack";
 import WelcomeMsg from "../components/WelcomeMsg";
 import Profile from "../components/Profile";
 import StudentInfo from '../components/StudentInfo';
+import { Upload } from "upload-js";
 
-const MentorDashboard = ({ theme }) => (
+const MentorDashboard = ({ theme }) => {
+		const [user, setUser] = React.useState({});
+		const [bio, setBio] = React.useState("");
+		const [avatarUrl, setAvatarUrl] = React.useState("");
+		const upload = Upload({ apiKey: "free" });
+
+		async function onFileSelected(event) {
+			const [file] = event.target.files;
+			const { fileUrl } = await upload.uploadFile(file, {
+				onBegin: ({ cancel }) => console.log("File upload started!"),
+				onProgress: ({ progress }) =>
+					console.log(`File uploading... ${progress}%`),
+			});
+			setAvatarUrl(fileUrl);
+		}
+		const getUserById = async () => {
+			try {
+				const user = JSON.parse(localStorage.getItem("user"));
+
+				const res = await fetch(`/api/users/${user.userId}`, {
+					headers: { authorization: `Bearer ${user.token}` },
+				});
+				const data = await res.json();
+				setUser(data);
+			} catch {
+				(error) => {
+					console.error(error);
+				};
+			}
+		};
+
+		React.useEffect(() => {
+			getUserById();
+		}, []);
+
+		// handle input change functions
+		const handleBioChange = (e) => {
+			setUser(user => ({...user, bio: e.target.value }))
+			
+		};
+		
+	return (
 	<ThemeProvider theme={theme}>
 		<Stack
 			sx={{
@@ -15,12 +57,12 @@ const MentorDashboard = ({ theme }) => (
 			}}
 			spacing={2}
 		>
-			<WelcomeMsg message="Welcome mentor name!👋" />
-			<Profile />
+			<WelcomeMsg message={`Welcome ${user.full_name}!👋`} />
+			<Profile bio={user.bio} handleBioChange={handleBioChange} avatar={avatarUrl || user.img_url} onFileSelected={onFileSelected} />
 			<StudentInfo />
-			
 		</Stack>
 	</ThemeProvider>
-);
+	)
+}
 export default MentorDashboard
 
