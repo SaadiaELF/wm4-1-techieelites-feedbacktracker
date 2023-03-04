42c39e5ae283c641e8aefcb6cf1be034d4ca6a67
Arbeits Sachen <arbeitsemail000@gmail.com>
2023-03-04 13:54:56 +0000

    Started About and added dummy text

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,14 +1,35 @@
-const About = () => (
-	<main role="main">
-		<div>
-			<h1>About</h1>
-			<p>
-				Starter kit for full-stack JavaScript projects. For more information,
-				see the wiki:
-			</p>
-			<a href="https://github.com/textbook/starter-kit/wiki">Wiki</a>
-		</div>
-	</main>
-);
+/* eslint-disable linebreak-style */
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-export default About;
+const theme = createTheme();
+
+export default function Album() {
+  return (
+    <ThemeProvider theme={theme}>
+		<CssBaseline />
+		<main>
+			{/* Hero unit */}
+			<Box sx={{ bgcolor: "background.paper", pt: 8, pb: 6 }}>
+				<Container maxWidth="sm">
+					<Typography component="h1" variant="h2" align="center" color="text.primary" gutterBottom>
+						About Us
+					</Typography>
+					<Typography variant="h5" align="center" color="text.secondary" paragraph>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At varius vel pharetra vel turpis nunc. Enim blandit volutpat maecenas volutpat blandit aliquam etiam erat.
+					</Typography>
+					<Stack sx={{ pt: 4 }} direction="row" spacing={2} justifyContent="center">
+						<Button variant="contained" sx={{ bgcolor: "red", ":hover":{ bgcolor:"black" } }}>Main Button</Button>
+						<Button variant="outlined" sx={{ bgcolor: "black", color: "white", ":hover":{ color:"black", border: "1px solid black" } }}>Secondary Button</Button>
+					</Stack>
+			</Container>
+			</Box>
+		</main>
+    </ThemeProvider>
+	);
+}
