e46e33880fa8db918f9ddfc8f1022c7642fc48c7
JanefrancessC <janefrancesschioma@gmail.com>
2023-03-06 20:01:55 +0000

    created the studentProfile

--- client/src/components/StudentProfileModal.js
+++ client/src/components/StudentProfileModal.js
@@ -1,11 +1,18 @@
 import * as React from "react";
-import { Button, ThemeProvider } from "@mui/material"
+import {
+	Button,
+	ThemeProvider,
+	CardContent,
+	Typography,
+	Card,
+} from "@mui/material";
 import Profile from "../components/Profile";
+import Progress from "../components/Progress";
 import { Stack } from "@mui/system";
 
 const StudentProfileModal = ({ student, theme }) => {
 	const [open, setOpen] = React.useState(false);
-  const [user, setUser] = React.useState({});
+	const [user, setUser] = React.useState({});
 
 	const handleClickOpen = () => {
 		setOpen(true);
@@ -22,14 +29,48 @@ const StudentProfileModal = ({ student, theme }) => {
 					maxWidth: "380px",
 					padding: "1rem",
 					margin: "auto",
+					marginBottom: "3rem",
 				}}
 				spacing={2}
 			>
-				<Profile
-					student={student}
-					bio={user.bio}
-					
-				/>
+				<Profile student={student} bio={user.bio} />
+			</Stack>
+			<Stack
+				spacing={2}
+				sx={{
+					maxWidth: "380px",
+					padding: "1rem",
+					margin: "auto",
+				}}
+			>
+				<Typography
+					variant="body1"
+					align="left"
+					sx={{ fontWeight: "bold", fontSize: "1.5rem" }}
+				>
+					Progress
+				</Typography>
+				<Card
+					sx={{
+						display: "flex",
+						flexDirection: "column",
+						backgroundColor: "#F2EFF0",
+						minHeight: 230,
+						width: "100%",
+					}}
+				>
+					<CardContent>
+						<Typography variant="body1" sx={{ fontWeight: "bold" }}>
+							Module/Lesson:{" "}
+						</Typography>
+						<Typography variant="body1" sx={{ fontWeight: "bold" }}>
+							softSkill:{" "}
+						</Typography>
+						<Typography variant="body1" sx={{ fontWeight: "bold" }}>
+							Feedback about the course:{" "}
+						</Typography>
+					</CardContent>
+				</Card>
 			</Stack>
 		</ThemeProvider>
 	);
