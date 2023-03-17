e14908ac496fc4ceda1dd2c4abde19c9f7ec2949
SaadiaELF <saadiaelfekak@gmail.com>
2023-03-17 20:52:21 +0000

    empty feedback case

--- client/src/components/MentorFeedback.js
+++ client/src/components/MentorFeedback.js
@@ -31,40 +31,56 @@ const MentorFeedBack = ({mentorId}) => {
 						display: "flex",
 						flexDirection: "column",
 						backgroundColor: "#F2EFF0",
-						minHeight: 200,
+						minHeight: 100,
 						width: "100%",
 					}}
 				>
 					<CardContent>
-						<Typography variant="body1" align="center" sx={{ fontWeight: 600, marginBottom:'1rem' }}>
+						<Typography
+							variant="body1"
+							align="center"
+							sx={{ fontWeight: 600, marginBottom: "1rem" }}
+						>
 							Mentor Feedback
 						</Typography>
-						{mentorData.module_type === "pd" ? (
-							<Typography variant="body2" sx={{ fontWeight: 600 }}>
-								SoftSkill:{}
+						{!mentorData.mentorId ? (
+							<Typography
+								variant="body2"
+								align="center"
+								sx={{ fontWeight: 300 }}
+							>
+								no recent feedback to show
 							</Typography>
 						) : (
 							<>
+								{mentorData.module_type === "pd" ? (
+									<Typography variant="body2" sx={{ fontWeight: 600 }}>
+										SoftSkill:{}
+									</Typography>
+								) : (
+									<>
+										<Typography variant="body2" sx={{ fontWeight: 600 }}>
+											Module/Lesson:
+										</Typography>
+										<Typography variant="body2" sx={{ fontWeight: 300 }}>
+											{mentorData.title}
+										</Typography>
+									</>
+								)}
 								<Typography variant="body2" sx={{ fontWeight: 600 }}>
-									Module/Lesson:
+									Mentor :
 								</Typography>
 								<Typography variant="body2" sx={{ fontWeight: 300 }}>
-									{mentorData.title}
+									{mentorData.full_name}
+								</Typography>
+								<Typography variant="body2" sx={{ fontWeight: 600 }}>
+									Feedback about the course :
+								</Typography>
+								<Typography variant="body2" sx={{ fontWeight: 300 }}>
+									{mentorData.text}
 								</Typography>
 							</>
 						)}
-						<Typography variant="body2" sx={{ fontWeight: 600 }}>
-							Mentor :
-						</Typography>
-						<Typography variant="body2" sx={{ fontWeight: 300 }}>
-							{mentorData.full_name}
-						</Typography>
-						<Typography variant="body2" sx={{ fontWeight: 600 }}>
-							Feedback about the course :
-						</Typography>
-						<Typography variant="body2" sx={{ fontWeight: 300 }}>
-							{mentorData.text}
-						</Typography>
 					</CardContent>
 				</Card>
 			</Stack>
